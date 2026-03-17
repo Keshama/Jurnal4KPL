a02f641cdfbcfec098e3aeca34f1fad16f19b0fa
Keshama <keisyaspw@gmail.com>
2026-03-17 07:31:33 +0700

    second commit

---
+++ Modul4_103022400060/KodePaket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul4_103022400060
+{
+    public class KodePaket
+    {
+        private Dictionary<string, string> tabelPaket = new Dictionary<string, string>()
+        {
+            {"Basic", "P201" },
+            {"Standard", "P202" },
+            {"Premium", "P203" },
+            {"Unlimited", "P204" },
+            {"Gaming", "P205" },
+            {"Streaming", "P206" },
+            {"Family", "P207" },
+            {"Business", "P208" },
+            {"Student", "P209" },
+            {"Traveler", "P210" }
+        };
+
+        public string getKodePaket (string namaPaket)
+        {
+            if (tabelPaket.ContainsKey(namaPaket))
+            {
+                return tabelPaket[namaPaket];
+            }
+            else
+            {
+                return "kode paket tidak ada";
+            }
+            
+        }
+    }
+}
--- Modul4_103022400060/Program.cs
+++ Modul4_103022400060/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul4_103022400060
+{
+    class Progeam
+    {
+        static void Main(string[] args)
+        {
+            KodePaket kode = new KodePaket();
+            string[] daftarPaket = {"Basic", "Standard", "Premium","bulanan", "Unlimited", "Streaming", "Family", "Business", "Student", "Traveler"};
+
+            foreach(string nama in daftarPaket)
+            {
+                Console.WriteLine($"Paket: {nama,-10} | Kode: {kode.getKodePaket(nama)}");
+            } 
+        }
+    }
+}
